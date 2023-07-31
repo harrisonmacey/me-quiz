c67438f6b5a4381a3bcd977390016dd779d312fa
hmacey <hmacey@students.cs.ubc.ca>
2023-07-31 11:51:26 -0700

    made all potential anwers from api

--- app/components/quiz/question.tsx
+++ app/components/quiz/question.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as he from "he";
+import shuffleArray from "@/helpers/shuffle-array";
 
 let userAnswers: number[] = [];
 
@@ -19,6 +20,7 @@ interface QuizQuestion {
   question: string;
   correct_answer: string;
   incorrect_answers: string[];
+  all_answers: string[];
 }
 
 const Question = ({ score, setScore, setFinished }: QuestionProps) => {
@@ -96,7 +98,7 @@ const Question = ({ score, setScore, setFinished }: QuestionProps) => {
     userAnswers.push(selectedAnswerIndex);
 
     // Set their score
-    if (selectedAnswerIndex === questions[currentQuestionIndex].correctAnswer) {
+    if (selectedAnswerIndex === correctAnswerIndex) {
       const newScore = score + 1;
       setScore(newScore);
     }
@@ -110,9 +112,24 @@ const Question = ({ score, setScore, setFinished }: QuestionProps) => {
     }
   };
 
-  const currentQuestion = questions[currentQuestionIndex];
+  //const currentQuestion = questions[currentQuestionIndex];
   let currentQuestion2: QuizQuestion;
   currentQuestion2 = questionsState[currentQuestionIndex];
+  let potentialAnswers = currentQuestion2 && currentQuestion2.incorrect_answers;
+  if (
+    potentialAnswers &&
+    !potentialAnswers.includes(currentQuestion2.correct_answer)
+  ) {
+    potentialAnswers.push(currentQuestion2.correct_answer);
+    potentialAnswers.forEach((e) => he.decode(e));
+    console.log(potentialAnswers);
+    shuffleArray(potentialAnswers);
+    console.log(potentialAnswers);
+    console.log("CORRECT ANSWER: " + currentQuestion2.correct_answer);
+  }
+  let correctAnswerIndex =
+    potentialAnswers &&
+    potentialAnswers.indexOf(currentQuestion2.correct_answer, 0);
 
   return (
     <div className="flex flex-col items-center gap-4 p-4 space-y-4">
@@ -120,21 +137,19 @@ const Question = ({ score, setScore, setFinished }: QuestionProps) => {
         Question {currentQuestionIndex + 1} of {numberOfQuestions}
       </span>
       <h2 className="text-2xl font-bold text-gray-700">
-        {currentQuestion.question}
-        <div></div>
-        {/* {currentQuestion2.question} */}
+        {currentQuestion2 && currentQuestion2.question}
       </h2>
       <div className="flex flex-col items-center w-full gap-1 space-y-4">
-        {/* {currentQuestion.incorrect_answers.map((answer, index) => ( */}
-        {currentQuestion.answers.map((answer, index) => (
-          <button
-            key={index}
-            onClick={() => handleAnswerClick(index)}
-            className="w-64 px-6 py-2 text-lg font-medium text-white transition-all duration-300 ease-in-out bg-blue-400 border border-blue-500 rounded-xl hover:bg-blue-500 active:bg-blue-500"
-          >
-            {answer}
-          </button>
-        ))}
+        {potentialAnswers &&
+          potentialAnswers.map((answer, index) => (
+            <button
+              key={index}
+              onClick={() => handleAnswerClick(index)}
+              className="w-64 px-6 py-2 text-lg font-medium text-white transition-all duration-300 ease-in-out bg-blue-400 border border-blue-500 rounded-xl hover:bg-blue-500 active:bg-blue-500"
+            >
+              {answer}
+            </button>
+          ))}
       </div>
     </div>
   );
